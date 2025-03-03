5ac29808e70eadc5238781683494de0f8d0a4b06
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-02 22:07:23 -1000

    Pass first test for calculator module

---
+++ calculator.test.js
@@ -0,0 +1,19 @@
+/**Calculator Tests
+ * Adds two numbers
+ * Subtracts two numbers
+ * Multiplies two numbers
+ * Divides two numbers
+ * Works on decimals
+ * Checks for type (x and y should be numbers, op should be a string)
+ */
+
+import calculator from "./calculator.js";
+
+// Test 1
+test("Adds two numbers together", () => {
+  expect(calculator(2, 2, "add")).toBe(4);
+  expect(calculator(-5, -2, "add")).toBe(-7);
+  expect(calculator(100, -20, "add")).toBe(80);
+  expect(calculator(0, 0, "add")).toBe(0);
+})
+
