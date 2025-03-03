0088059d88b5eba02fcfb2eb0ea710c73094971e
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-02 18:24:28 -1000

    Modify capitalize function to check for string

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,8 @@
 export default function capitalize(string) {
+  if (typeof(string) !== "string") {
+    return "Not a string.";
+  }
+
   let stringArray = string.trim().split("");
   stringArray[0] = stringArray[0].toUpperCase();
   return stringArray.join("");
--- capitalize.test.js
+++ capitalize.test.js
@@ -10,11 +10,16 @@
 import capitalize from "./capitalize.js"
 
 test("Capitalizes the first letter in a basic string", () => {
-  let testString = "the wheels on the bus go round and round."
+  let testString = "the wheels on the bus go round and round.";
   expect(capitalize(testString)).toBe("The wheels on the bus go round and round.")
 })
 
 test("Capitalizes the first letter despite excessive spaces", () => {
-  let testString = "    the wheels on the bus go round and round."
+  let testString = "    the wheels on the bus go round and round.";
   expect(capitalize(testString)).toBe("The wheels on the bus go round and round.")
+})
+
+test("Checks if it's a string", () => {
+  let testObject = {name: "name"};
+  expect(capitalize(testObject)).toBe("Not a string.");
 })
