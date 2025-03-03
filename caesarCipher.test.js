b4902075d977bccc2c1d56e48f1814c80cfa52f9
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-03 01:56:48 -1000

    Add test ideas to caesarCipher test file

---
+++ caesarCipher.test.js
@@ -0,0 +1,7 @@
+/**Caesar Cipher Test
+ * Ciphers a lower-case letter-only string
+ * Ciphers text that wraps (goes from z to a);
+ * Ciphers a lower and upper-case letter-only string
+ * Ciphers a lower-case number & symbol containing string
+ * Ciphers a lower and upper-case number & symbol containing string
+ */
