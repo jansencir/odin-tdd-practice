ca97aee309fad52f53a2c2373de4b83fc2dace70
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-02 18:16:59 -1000

    Create capitalize function to pass first test

---
+++ capitalize.test.js
@@ -0,0 +1,15 @@
+/**Capitalize Tests
+ * Capitalizes the first letter
+ * Excessive spaces
+ * Not a string
+ * First letter is already capital
+ * First character is a number
+ * First character is a symbol
+ */
+
+import capitalize from "./capitalize.js"
+
+test("Capitalizes the first letter in a basic string", () => {
+  let testString = "the wheels on the bus go round and round."
+  expect(capitalize(testString)).toBe("The wheels on the bus go round and round.")
+})
