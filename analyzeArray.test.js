6d9d8af226b86d09048c482b502a1f7fbb18508e
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-03 21:34:21 -1000

    Add initial test ideas for analyze array

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,4 +1,6 @@
 /**Analyze Array Tests
  * Returns an object containing: average, min, max, & length
  * Returns 0 for everything if passed an empty array
+ * Works on an array of negative and positive numbers
+ * Works on an array of just negative numbers
  */
