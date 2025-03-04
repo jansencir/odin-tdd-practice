e29b16c832535d2072b862ed02083c9b4c7df1de
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-03-03 22:40:45 -1000

    Pass first test for analyze array

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -4,3 +4,13 @@
  * Works on an array of negative and positive numbers
  * Works on an array of just negative numbers
  */
+
+import analyzeArray from "./analyzeArray.js";
+
+// Test 1 - Returns an object containing; average, min, max, & length
+test("Works on a simple array", () => {
+  let testArray = [5, 2, 4, 3, 1]
+  expect(analyzeArray(testArray)).toEqual(
+    { average: 3, min: 1, max: 5, length: 5 }
+  )
+})
